ff58d90a3a7b3a2de5316b9a9a79c2e3966bf877
Maciej Misztal <mmisztal1980@gmail.com>
2016-05-19 13:05:07 +0200

    project added

---
+++ src/Example.Api/App_Start/WebApi.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+using Owin;
+using Swashbuckle.Application;
+
+namespace Example.Api
+{
+    public static class WebApi
+    {
+        public static IAppBuilder UseWebApi(this IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+
+            // Enable Json response 
+            configuration.Formatters
+                .JsonFormatter
+                .SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
+            configuration
+                .EnableSwagger(c => c.SingleApiVersion("v1", "Example DevCamp2016 API"))
+                .EnableSwaggerUi("swagger/docs/{apiVersion}");
+
+            app.UseWebApi(configuration);
+
+            return app;
+        }
+    }
+}
---
+++ src/Example.Api/IISStartup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+// http://www.asp.net/aspnet/overview/owin-and-katana/owin-startup-class-detection
+[assembly: OwinStartup("IIS-Startup", typeof(Example.Api.IISStartup))]
+
+namespace Example.Api
+{
+    public class IISStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app
+                .UseWebApi()
+                .Use(async (ctx, next) =>
+                {
+                    if (ctx.Request.Path.Value.Equals("/"))
+                    {
+                        ctx.Response.Redirect("swagger/docs/v1");
+                        return;
+                    }
+
+                    await next();
+                });
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
