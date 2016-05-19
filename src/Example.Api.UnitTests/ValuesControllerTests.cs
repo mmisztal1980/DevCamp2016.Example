ddd2ee72f5a06ad720f86368f3f130a1ab0c6820
Maciej Misztal <mmisztal1980@gmail.com>
2016-05-19 13:04:41 +0200

    tests added

---
+++ src/Example.Api.UnitTests/ValuesControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Example.Api.Controllers;
+using Xunit;
+
+namespace Example.Api.UnitTests
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void Get_Id_ValueString()
+        {
+            // Arrange
+            var ctrl = new ValuesController();
+
+            // Act
+            var response = ctrl.Get(6);
+
+            // Assert
+            Assert.Equal("value", response);
+        }
+    }
+}
